6e688d768946d7900c05023862ca43498130ac17
TheMetalStorm <arapoglu.simon@gmx.de>
2025-07-05 20:03:35 +0200

    first commit

---
+++ src/main/java/com/themetalstorm/bibliothekssystem/BibliothekssystemApplication.java
@@ -0,0 +1,13 @@
+package com.themetalstorm.bibliothekssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothekssystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliothekssystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/themetalstorm/bibliothekssystem/BibliothekssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themetalstorm.bibliothekssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothekssystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
