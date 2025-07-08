0dda6477c6b6d134fd9f248fd65dda5e8e22b1ac
TheMetalStorm <arapoglu.simon@gmx.de>
2025-07-08 04:26:45 +0200

    implemented basic user auth and started work on auth with JWT

--- src/main/java/com/themetalstorm/bibliothekssystem/config/SecurityConfig.java
+++ src/main/java/com/themetalstorm/bibliothekssystem/config/SecurityConfig.java
@@ -1,38 +1,38 @@
 package com.themetalstorm.bibliothekssystem.config;
 
-import com.themetalstorm.bibliothekssystem.service.MyUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private MyUserService userDetailsService ;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("register", "login")
+                        .permitAll()
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
+
 
     @Bean
     public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
@@ -40,5 +40,10 @@ public class SecurityConfig {
         authProvider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         authProvider.setUserDetailsService(userDetailsService);
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/com/themetalstorm/bibliothekssystem/controller/UserController.java
+++ src/main/java/com/themetalstorm/bibliothekssystem/controller/UserController.java
@@ -3,21 +3,25 @@ package com.themetalstorm.bibliothekssystem.controller;
 import com.themetalstorm.bibliothekssystem.model.User;
 import com.themetalstorm.bibliothekssystem.service.MyUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
-    private final MyUserService userDetailsService;
+    private final MyUserService userService;
 
     @Autowired
-    public UserController(MyUserService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    public UserController(MyUserService userService) {
+        this.userService = userService;
     }
 
 
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
-        return userDetailsService.registerUser(user);
-
+        return userService.register(user);
+    }
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+        return userService.verify(user);
     }
 }
