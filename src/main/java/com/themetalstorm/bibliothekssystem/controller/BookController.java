0e8c787f50f1f49baf82a3eaa6488b75c23de8c8
TheMetalStorm <arapoglu.simon@gmx.de>
2025-07-09 22:06:53 +0200

    added Sort and Pagination to search endpoint

--- src/main/java/com/themetalstorm/bibliothekssystem/controller/BookController.java
+++ src/main/java/com/themetalstorm/bibliothekssystem/controller/BookController.java
@@ -3,6 +3,7 @@ package com.themetalstorm.bibliothekssystem.controller;
 import com.themetalstorm.bibliothekssystem.service.BookService;
 import com.themetalstorm.bibliothekssystem.dto.BookDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,11 +29,16 @@ public class BookController {
     }
 
     @GetMapping("/search")
-    List<BookDTO> findBySearch(@RequestParam(required = false) String name,
+    Page<BookDTO> findBySearch(@RequestParam(required = false) String name,
                                @RequestParam(required = false) Integer genreId,
-                               @RequestParam(required = false) Integer authorId)
-         {
-        return bookService.getBookBySearch(name, genreId, authorId);
+                               @RequestParam(required = false) Integer authorId,
+                               @RequestParam(defaultValue = "0") int page,
+                               @RequestParam(defaultValue = "10") int size,
+                               @RequestParam(defaultValue = "name") String sortField,
+                               @RequestParam(defaultValue = "ASC") String sortDirection) {
+
+
+        return bookService.getBookBySearch(name, genreId, authorId, page, size, sortField, sortDirection);
     }
 
     @PostMapping("")
