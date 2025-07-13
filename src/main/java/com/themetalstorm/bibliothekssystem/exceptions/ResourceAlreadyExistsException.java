c0affc1f2cefcdebd095f713b266b81bfdf65851
simon <you@example.com>
2025-07-13 02:26:52 +0200

    added resource already exists exception for better user error messages

--- src/main/java/com/themetalstorm/bibliothekssystem/exceptions/ResourceAlreadyExistsException.java
+++ src/main/java/com/themetalstorm/bibliothekssystem/exceptions/ResourceAlreadyExistsException.java
@@ -1,7 +1,11 @@
 package com.themetalstorm.bibliothekssystem.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class ResourceAlreadyExistsException extends RuntimeException {
-  public ResourceAlreadyExistsException(String message) {
-    super(message);
-  }
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
 }
