6883998e2fa35d4af92c940d869143bac7207782
simon <you@example.com>
2025-07-13 02:25:40 +0200

    added put method for author, genre, book, user

--- src/main/java/com/themetalstorm/bibliothekssystem/controller/AuthorController.java
+++ src/main/java/com/themetalstorm/bibliothekssystem/controller/AuthorController.java
@@ -1,12 +1,14 @@
 package com.themetalstorm.bibliothekssystem.controller;
 
 import com.themetalstorm.bibliothekssystem.dto.AuthorDTO;
+import com.themetalstorm.bibliothekssystem.dto.BookDTO;
+import com.themetalstorm.bibliothekssystem.dto.GenreDTO;
 import com.themetalstorm.bibliothekssystem.service.AuthorService;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-//TODO: return Response Entity when appropriate
 
 @RestController
 @RequestMapping("/authors")
@@ -19,21 +21,35 @@ public class AuthorController {
     }
 
     @GetMapping("")
-    public Page<AuthorDTO> getAllAuthors(@RequestParam(required = false) Integer page,
-                                         @RequestParam(required = false) Integer size,
-                                         @RequestParam(defaultValue = "lastName") String sortField,
-                                         @RequestParam(defaultValue = "ASC") String sortDirection)  {
-        return authorService.getAllAuthors(page, size, sortField, sortDirection);
+    public ResponseEntity<Page<AuthorDTO>> getAllAuthors(@RequestParam(required = false) Integer page,
+                                                         @RequestParam(required = false) Integer size,
+                                                         @RequestParam(defaultValue = "lastName") String sortField,
+                                                         @RequestParam(defaultValue = "ASC") String sortDirection)  {
+        return ResponseEntity.ok(authorService.getAllAuthors(page, size, sortField, sortDirection));
     }
 
     @GetMapping("/{id}")
-    public AuthorDTO getAuthor(@PathVariable int id) {
-        return authorService.getAuthorById(id);
+    public ResponseEntity<AuthorDTO> getAuthor(@PathVariable int id) {
+        return ResponseEntity.ok(authorService.getAuthorById(id));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("")
+    @ResponseBody
+    ResponseEntity<AuthorDTO> addAuthor(@RequestBody AuthorDTO book) {
+        return new ResponseEntity<>(authorService.saveAuthor(book), HttpStatus.CREATED);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("/{id}")
+    public ResponseEntity<AuthorDTO> updateAuthor(@PathVariable int id, @RequestBody AuthorDTO authorDTO) {
+        return ResponseEntity.ok(authorService.updateAuthor(id, authorDTO));
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
-    public void deleteAuthor(@PathVariable int id) {
+    public ResponseEntity<Void> deleteAuthor(@PathVariable int id) {
         authorService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
